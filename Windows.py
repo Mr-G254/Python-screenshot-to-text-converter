42036a5fbdf3f53860f8a5d1d4c08c616c47168c
Ezekiel  G <75112735+Mr-G254@users.noreply.github.com>
2023-07-06 02:32:54 +0300

    UI

--- Windows.py
+++ Windows.py
@@ -7,48 +7,57 @@ class Image_Frame():
 
         self.img0 = CTkImage(Image.open("Icons\\ad_image.png"),size=(22,22))
         self.img1 = CTkImage(Image.open("Icons\capture.png"),size=(23,23))
+        self.img2 = CTkImage(Image.open("Icons\copy.png"),size=(23,23))
+        self.img3 = CTkImage(Image.open("Icons\clear.png"),size=(23,23))
+        self.img4 = CTkImage(Image.open("Icons\convert.png"),size=(23,23))
+        self.img5 = CTkImage(Image.open("Icons\\next.png"),size=(22,22))
 
         self.Frame = CTkFrame(self.master,width=1054,height=600,fg_color="#58001D",corner_radius=0)
 
         self.text = CTkLabel(self.Frame,text="Convert images to text(Lyrics)",width=1100,height=40,fg_color="#58001D",font=('Times',19))
         self.text.place(x=-46,y=0)
 
-        self.browse = CTkButton(self.Frame,width=140,height=32,text="Browse",anchor=W,font=('Times',14),image=self.img0,compound=LEFT,fg_color="#4F0018",border_width=0,border_color="#790028",corner_radius=6)
+        self.browse = CTkButton(self.Frame,width=140,height=32,text="Browse",anchor=W,font=('Times',16),image=self.img0,compound=LEFT,fg_color="#790028",border_width=0,hover_color="#4F0018",corner_radius=6)
         self.browse.place(x=67,y=40)
-        self.browse.bind('<Enter>', lambda event: self.highlight_button(event,self.browse))
-        self.browse.bind('<Leave>', lambda event: self.unhighlight_button(event,self.browse))
 
-        self.screen = CTkButton(self.Frame,width=140,height=32,text="Screenshot",anchor=E,font=('Times',14),image=self.img1,compound=RIGHT,fg_color="#4F0018",border_width=0,border_color="#790028",corner_radius=6)
+        self.screen = CTkButton(self.Frame,width=140,height=32,text="Screenshot",anchor=E,font=('Times',16),image=self.img1,compound=RIGHT,fg_color="#790028",border_width=0,hover_color="#4F0018",corner_radius=6)
         self.screen.place(x=212,y=40)
-        self.screen.bind('<Enter>', lambda event: self.highlight_button(event,self.screen))
-        self.screen.bind('<Leave>', lambda event: self.unhighlight_button(event,self.screen))
-
+       
         self.frame_1  = CTkFrame(self.Frame,width=400,height=510,fg_color="#790028",corner_radius=10)
         self.frame_1.place(x=12,y=80)
 
-        self.text_1 = CTkLabel(self.frame_1,text="Images",width=390,height= 25,fg_color="#790028",font=('Times',17))
+        self.text_1 = CTkLabel(self.frame_1,text="Images",width=390,height= 25,fg_color="#790028",font=('Times',18))
         self.text_1.place(x=5,y=5)
 
         img_frame = CTkScrollableFrame(self.frame_1,width=370,height=455,fg_color="#4F0018",corner_radius=10)
         img_frame.place(x=2,y=33)
 
+        self.copy = CTkButton(self.Frame,width=120,height=32,text="Copy",anchor=W,font=('Times',16),compound=LEFT,image=self.img2,fg_color="#790028",border_width=0,hover_color="#4F0018",corner_radius=6)
+        self.copy.place(x=760,y=40)
+
+        self.clear = CTkButton(self.Frame,width=120,height=32,text="Clear",anchor=E,font=('Times',16),compound=RIGHT,image=self.img3,fg_color="#790028",border_width=0,hover_color="#4F0018",corner_radius=6)
+        self.clear.place(x=885,y=40)
+
         self.frame_2  = CTkFrame(self.Frame,width=320,height=510,fg_color="#790028",corner_radius=10)
         self.frame_2.place(x=720,y=80)
 
-        self.text_2 = CTkLabel(self.frame_2,text="Text",width=310,height= 25,fg_color="#790028",font=('Times',17))
+        self.text_2 = CTkLabel(self.frame_2,text="Text",width=310,height= 25,fg_color="#790028",font=('Times',18))
         self.text_2.place(x=5,y=5)
 
-        lyr_frame = CTkFrame(self.frame_2,width=316,height=475,fg_color="#4F0018",corner_radius=10)
-        lyr_frame.place(x=2,y=33)
+        self.lyr_frame = CTkFrame(self.frame_2,width=316,height=475,fg_color="#4F0018",corner_radius=10)
+        self.lyr_frame.place(x=2,y=33)
+
+        self.text_area = CTkTextbox(self.lyr_frame,width=311,height=465,font=('Times',19),wrap='word',border_spacing=0,fg_color="#4F0018",corner_radius=10)
+        self.text_area.place(x=2,y=2)
+
+        self.conv = CTkButton(self.Frame,width=150,height=32,text="Convert",font=('Times',16),compound=RIGHT,fg_color="#790028",border_width=0,hover_color="#4F0018",corner_radius=6)
+        self.conv.place(x=491,y=521)
+
+        self.next = CTkButton(self.Frame,width=150,height=32,text="Next",font=('Times',16),compound=RIGHT,fg_color="#790028",border_width=0,hover_color="#4F0018",corner_radius=6)
+        self.next.place(x=491,y=558)
 
     def place(self):
         self.Frame.place(x=46,y=0)
 
     def hide(self):
         self.Frame.place_forget()
-
-    def highlight_button(self,event,button):
-        button.configure(border_width=2)
-
-    def unhighlight_button(self,event,button):
-        button.configure(border_width=0)
